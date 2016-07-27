3ea9a1f3814bb115316ff1660e8e4877a0d4b215
tomandhua <903696213@qq.com>
2016-07-27 22:20:40 +0800

    create a autosize project

---
+++ 05 Autosize/05 Autosize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05 Autosize
+//
+//  Created by tomandhua on 16/6/1.
+//  Copyright © 2016年 tomandhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05 Autosize/05 Autosize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  05 Autosize
+//
+//  Created by tomandhua on 16/6/1.
+//  Copyright © 2016年 tomandhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
